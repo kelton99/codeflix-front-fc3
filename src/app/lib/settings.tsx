eb73edd5e6cb68bc7778e2694bfe421d995ba0a0
ksf <ksf@localhost.localdomain>
2024-01-15 15:17:26 -0300

    starting login page

---
+++ src/app/lib/settings.tsx
@@ -0,0 +1,29 @@
+export const getAppSettings = (): Promise<{
+  theme: string;
+  language: string;
+}> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        theme: 'dark',
+        language: 'en',
+      });
+    }, 1000);
+  });
+};
+
+export const getUserInfo = (): Promise<{
+  name: string;
+  email: string;
+  age: number;
+}> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        name: 'John Doe',
+        email: 'jhon@.com',
+        age: 30,
+      });
+    }, 1000);
+  });
+};
---
+++ src/app/settings/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function error({ error, reset }: any) {
+  useEffect(() => {
+    console.log('logging error:', error);
+  }, [error]);
+
+  return (
+    <div className='text-red-500'>
+      <h1 className='text-2xl font-bold'>Error Page</h1>
+      <div className='border-shaded border border-red-500 p-4'>
+        <p>Something went wrong</p>
+      </div>
+    </div>
+  );
+}
