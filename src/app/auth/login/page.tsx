622fe51c63b577735e7ae63f3aa9b89bb2805f95
ksf <ksf@localhost.localdomain>
2024-01-16 10:27:40 -0300

    login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,12 +1,5 @@
-export default function Login() {
-  return (
-    <div
-      style={{ backgroundImage: 'url(/background.jpg)' }}
-      className='bg-cover bg-center bg-no-repeat text-white'
-    >
-      <div className='flex min-h-screen flex-col items-center justify-center py-2'>
-        Página de Login
-      </div>
-    </div>
-  );
+import LoginForm from './Login';
+
+export default function LoginPage() {
+  return <LoginForm />;
 }
