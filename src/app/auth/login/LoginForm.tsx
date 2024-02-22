de9243ea5436be37f5b17fcd69d76e9a141b88cc
ksf <ksf@localhost.localdomain>
2024-02-22 13:01:17 -0300

    login validation

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,12 +1,41 @@
 'use client';
 
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { AuthForm } from '@/app/components/AuthForm';
+import { useRouter } from 'next/navigation';
+
+type ServerError = {
+  message: string;
+};
 
 export default function LoginForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log('submit');
+  const router = useRouter();
+  const [errors, setErrors] = useState<String[]>([]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+        return;
+      }
+
+      const payload: ServerError[] = await response.json();
+      setErrors(payload.map((error) => error.message));
+    } catch (error) {
+      console.error(error);
+      setErrors(['An unknow error occurred.']);
+    }
   };
   return <AuthForm formType='login' onSubmit={handleSubmit} />;
 }
