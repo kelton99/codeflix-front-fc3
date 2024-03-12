bc8ff080ac2b67b360af3fdbbccc9839fab611bc
kelton99 <keltonfreitas53@gmail.com>
2024-03-12 11:21:26 -0300

    banner from api

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,32 +1,43 @@
 import Image from 'next/image';
 import { InformationCircleIcon } from '@heroicons/react/24/outline';
 import { PlayIcon } from '@heroicons/react/24/solid';
+import { Movie } from '../types/movie';
+import Link from 'next/link';
 
-export default function Banner() {
+export default function Banner({ movie }: { movie: Movie }) {
   return (
     <div className='mb-10 lg:mb-20'>
       <div className='flex flex-col space-y-4 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
         <div className='absolute left-0 top-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
+          <video
+            autoPlay
+            loop
+            muted
+            className='z-20 hidden h-full w-full object-cover opacity-50 transition duration-1000 ease-in-out lg:block'
+            poster={movie.bannerFileURL}
+            src={movie.videoFileURL}
+          />
           <Image
-            src='/banner.jpg'
-            alt='Star Trek'
+            src={movie.bannerFileURL}
+            alt={movie.title}
             fill={true}
-            className='h-[65vh] object-cover object-top lg:h-[95vh]'
+            className='object-cover object-top opacity-30 filter lg:hidden'
           />
         </div>
 
-        <h1 className='text-4xl font-bold lg:text-7xl'>Star Trek</h1>
+        <h1 className='text-4xl font-bold lg:text-7xl'>{movie.title}</h1>
         <p className='text-shadow-md text-sm text-gray-300 md:max-w-lg md:text-lg lg:max-w-2xl'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum quidem
-          soluta sequi facilis maiores perspiciatis laudantium ad nostrum.
+          {movie.description}
         </p>
       </div>
-
+      <div className='flex space-x-3'></div>
       <div className='flex space-x-3'>
+        <Link href={`/watch/${movie.id}`}>
         <button className='flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5'>
           <PlayIcon className='h-6' />
-          Play
+          Watch Now
         </button>
+        </Link>
         <button className='flex cursor-pointer items-center gap-x-2 rounded bg-gray-500 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8'>
           <InformationCircleIcon className='h-6' />
           More Info
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from './components/Header';
 import MovieRow from './components/MovieRow';
 import Banner from './components/Banner';
+import { getFeatureMovie } from './service/MovieService';
 
-export default function Home() {
+export default async function Home() {
+  const featuredMovie = await getFeatureMovie('101');
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header />
       <main className='relative overflow-y-scroll p-8 pb-20 scrollbar-hide lg:px-16 '>
-        <Banner />
+        <Banner movie={featuredMovie} />
         <MovieRow sectionTitle='Trending now' />
         <MovieRow sectionTitle='Top Rated' />
         <MovieRow sectionTitle='Action Movies' />
